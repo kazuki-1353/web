9da3f008e7474ba60f2799b5f183e196e55f51db
dsunm-kun-he <kun_he@c.dwango.co.jp>
2024-12-27 15:10:20 +0900

    a

--- utils/funBatch.ts
+++ utils/funBatch.ts
@@ -1,8 +1,71 @@
 /** 分批处理函数 */
-module.exports = async (tasks: Function[], size = 2) => {
-  for (let i = 0; i < tasks.length; i += size) {
-    const batch = tasks.slice(i, i + size); // 按批次分割
-    await Promise.all(batch.forEach(task)); // 执行批次内的任务
-    console.log(`Processed batch ${Math.floor(i / size) + 1}`);
+const funBatch = async <T, R>(
+  list: T[],
+  task: (item: T, index: number, batch: T[], batchIndex: number) => Promise<R>,
+  options?: {
+    size?: number;
+    then?: (res: {
+      processed: number;
+      result: PromiseSettledResult<R>[];
+    }) => void;
+  },
+): Promise<void> => {
+  const { length } = list;
+  const { size = 2, then } = options || {};
+
+  for (let index = 0; index < length; index += size) {
+    const endIndex = index + size;
+    const batch = list.slice(index, endIndex); // 按批次分割
+    const result = await Promise.allSettled(
+      batch.map(async (data, batchIndex) => {
+        return await task(data, index + batchIndex, batch, batchIndex); // 执行批次内的任务
+      }),
+    );
+
+    const processed =
+      endIndex < length ? Math.round((endIndex / length) * 10000) / 100 : 100;
+
+    then?.({
+      processed,
+      result,
+    });
   }
 };
+
+module.exports = funBatch;
+
+/* TEST */
+// const proms = Array(9).fill(null);
+// const fun = (data: number) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(data);
+//       console.log(data);
+//     }, 1000);
+//   });
+// };
+// funBatch(
+//   proms,
+//   async (_, k) => {
+//     return fun(k);
+//   },
+//   {
+//     then(res) {
+//       console.log(res);
+//     },
+//   },
+// );
+// funBatch(
+//   proms,
+//   async (_, k, b, bi) => {
+//     if (bi === 0) {
+//       return await fun(k);
+//     }
+//   },
+//   {
+//     then(res) {
+//       console.log(res);
+//     },
+//   },
+// );
+/* TEST */
