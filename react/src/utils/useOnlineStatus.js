19fed1e1f592691f813bcb6f637bcdc27163cfe5
KAZUKI <376073739@qq.com>
2026-05-31 01:42:14 +0900

    z

---
+++ react/src/utils/useOnlineStatus.js
@@ -0,0 +1,24 @@
+import { useSyncExternalStore } from 'react';
+
+export function useOnlineStatus() {
+  const isOnline = useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
+  return isOnline;
+}
+
+/* 创建稳定的函数避免重新订阅 */
+function subscribe(callback) {
+  window.addEventListener('online', callback);
+  window.addEventListener('offline', callback);
+  return () => {
+    window.removeEventListener('online', callback);
+    window.removeEventListener('offline', callback);
+  };
+}
+
+function getSnapshot() {
+  return navigator.onLine;
+}
+
+function getServerSnapshot() {
+  return true; // 服务端生成的 HTML 总是显示“在线”
+}
